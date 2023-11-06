96a1ea4c1af5d5f3bad965ac4ae16a18e4e211c7
stge35 <evanok1@gmail.com>
2023-11-06 19:18:01 +0900

    first commit

---
+++ src/main/java/practice/mockito/PracticeMockitoApplication.java
@@ -0,0 +1,13 @@
+package practice.mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeMockitoApplication.class, args);
+	}
+
+}
